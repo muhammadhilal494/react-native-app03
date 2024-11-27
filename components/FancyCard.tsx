34737c7c3685165f11176b0916611afe967eace1
Muhammad Hilal <muhammadhilal494513@gmail.com>
2024-11-27 15:23:22 +0500

    Image, FancyCard

---
+++ components/FancyCard.tsx
@@ -0,0 +1,38 @@
+import { Image, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function FancyCard() {
+  return (
+    <View>
+      <Text style={styles.headingText}>Trending places</Text>
+      <View style={[styles.card, styles.cardElevated]}>
+        <Image
+        source={{
+            uri: 'https://images.pexels.com/photos/3581369/pexels-photo-3581369.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            }}
+        style={styles.cardImage}
+        />
+        <View style={styles.cardBody}>
+            <Text style={styles.cardTitle}>Hawa Mahal</Text>
+            <Text style={styles.cardLabel}>Pink City, jaipur</Text>
+            <Text style={styles.cardDescription}>The Hawa Mahal is a palace in the city of Jaipur, India. Built from
+            red and pink sandstone, it is on the edge of the City Palace.</Text>
+            <Text style={styles.cardFooter}>12 mins away</Text>
+        </View>
+      </View>
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+    headingText: {},
+    card: {},
+    cardElevated: {},
+    cardImage: {
+        height: 180
+    },
+    cardBody: {},
+    cardTitle: {},
+    cardLabel: {},
+    cardDescription: {},
+    cardFooter: {},
+})
