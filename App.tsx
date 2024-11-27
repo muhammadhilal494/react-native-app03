a0f0e684577f9ab0021d7f8abbf401b1a8c643d2
Muhammad Hilal <muhammadhilal494513@gmail.com>
2024-11-27 22:02:54 +0500

    Styling to FancyCard

--- App.tsx
+++ App.tsx
@@ -11,6 +11,7 @@ const App = () => {
         <FlatCards />
         <ElevatedCards />
         <FancyCard />
+        <FancyCard />
       </ScrollView>
     </SafeAreaView>
   )
--- components/FancyCard.tsx
+++ components/FancyCard.tsx
@@ -24,15 +24,59 @@ export default function FancyCard() {
   )
 }
 const styles = StyleSheet.create({
-    headingText: {},
-    card: {},
-    cardElevated: {},
-    cardImage: {
-        height: 180
+    headingText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        paddingHorizontal: 8
     },
-    cardBody: {},
-    cardTitle: {},
-    cardLabel: {},
-    cardDescription: {},
-    cardFooter: {},
+    card: {
+        width: 335,
+        height: 360,
+        borderRadius: 6,
+        marginVertical: 12,
+        marginHorizontal: 10
+        
+    },
+    cardElevated: {
+        backgroundColor: '#FFFFFF',
+        elevation: 3,
+        shadowOffset: {
+        width: 1,
+        height: 1
+      }
+      
+    },
+    cardImage: {
+        height: 180,
+        marginBottom: 8,
+        borderTopLeftRadius: 6,
+        borderTopRightRadius: 6
+      
+    },
+    cardBody: {
+        flex: 1,
+        flexGrow: 1,
+        paddingHorizontal: 12
+    },
+    cardTitle: {
+        color: '#000000',
+        fontSize: 22,
+        fontWeight: 'bold',
+        marginBottom: 4
+    },
+    cardLabel: {
+        color: '#000000',
+        fontSize: 14,
+        marginBottom: 6
+    },
+    cardDescription: {
+        color: '#242B2E',
+        fontSize: 12,
+        marginBottom: 12,
+        marginTop: 6,
+        flexShrink: 1
+    },
+    cardFooter: {
+        color: '#000000'
+    },
 })
