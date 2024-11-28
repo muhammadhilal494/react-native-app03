b08c866e6a4c70fb4a626947294e003f120a50e8
Muhammad Hilal <muhammadhilal494513@gmail.com>
2024-11-28 09:37:07 +0500

    WhatsApp chat style design

--- components/ContactList.tsx
+++ components/ContactList.tsx
@@ -55,14 +55,37 @@ export default function ContactList() {
 }
 
 const styles = StyleSheet.create({
-    headingText: {},
-    container: {},
-    userCard: {},
+    headingText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        paddingHorizontal: 8
+    },
+    container: {
+        paddingHorizontal: 10,
+        marginBottom: 4
+        
+    },
+    userCard: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 3,
+        backgroundColor: '#8D3DAF',
+        padding: 8,
+        borderRadius: 10
+    },
     userImage: {
         width: 60,
         height: 60,
         borderRadius: 60 / 2,
+        marginRight: 14
     },
-    userName: {},
-    userStatus: {}
+    userName: {
+        fontSize: 16,
+        fontWeight: '600',
+        color: '#FFF'
+    },
+    userStatus: {
+        fontSize: 12
+    }
 })
